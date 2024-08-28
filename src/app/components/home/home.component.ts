e1287736e4ff88d2e81dde8eff918432d154f0b0
M.can <caglayan.mertcan37@gmail.com>
2024-08-28 18:32:34 +0300

    add main section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { TabbarComponent } from '../tabbar/tabbar.component';
-import { SliderComponent } from "../slider/slider.component";
+
+import { MainSectionComponent } from './main-section/main-section.component';
+import { SliderComponent } from './slider/slider.component';
+import { TabbarComponent } from './tabbar/tabbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TabbarComponent, SliderComponent],
+  imports: [MainSectionComponent, SliderComponent, TabbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
