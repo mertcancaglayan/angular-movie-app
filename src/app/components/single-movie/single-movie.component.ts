299334d3742d9163eaee62245a962ae77176ca7f
M.can <caglayan.mertcan37@gmail.com>
2024-09-03 16:16:10 +0300

    create header component

--- src/app/components/single-movie/single-movie.component.ts
+++ src/app/components/single-movie/single-movie.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../layouts/header/header.component';
 
 @Component({
   selector: 'app-single-movie',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './single-movie.component.html',
-  styleUrl: './single-movie.component.scss'
+  styleUrl: './single-movie.component.scss',
 })
 export class SingleMovieComponent {
-
+  title: string = 'Detail';
 }
--- src/app/components/watch-list/watch-list.component.ts
+++ src/app/components/watch-list/watch-list.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { TabbarComponent } from "../tabbar/tabbar.component";
+import { TabbarComponent } from '../tabbar/tabbar.component';
+import { HeaderComponent } from '../layouts/header/header.component';
 
 @Component({
   selector: 'app-watch-list',
   standalone: true,
-  imports: [TabbarComponent],
+  imports: [TabbarComponent, HeaderComponent],
   templateUrl: './watch-list.component.html',
-  styleUrl: './watch-list.component.scss'
+  styleUrl: './watch-list.component.scss',
 })
 export class WatchListComponent {
-
+  title: string = 'Watch list';
 }
