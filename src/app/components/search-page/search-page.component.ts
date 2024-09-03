5850b038f2602495d5f09178d3bd63e80c5e9866
M.can <caglayan.mertcan37@gmail.com>
2024-09-03 16:08:31 +0300

    create movie-card component

--- src/app/components/search-page/search-page.component.ts
+++ src/app/components/search-page/search-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from "../search-bar/search-bar.component";
 import { TabbarComponent } from "../tabbar/tabbar.component";
+import { MovieCardComponent } from "../movie-card/movie-card.component";
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [SearchBarComponent, TabbarComponent],
+  imports: [SearchBarComponent, TabbarComponent, MovieCardComponent],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss'
 })
