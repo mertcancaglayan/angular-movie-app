cf9b37dc96854fe73b46384a130c71e346199635
M.can <caglayan.mertcan37@gmail.com>
2024-09-04 21:40:42 +0300

    update loading screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
-import { LoadingScreenComponent } from "./components/loading-screen/loading-screen.component";
-import { HomeComponent } from "./components/home/home.component";
-import { SearchPageComponent } from "./components/search-page/search-page.component";
-import { SingleMovieComponent } from "./components/single-movie/single-movie.component";
-import { WatchListComponent } from "./components/watch-list/watch-list.component";
+import { LoadingScreenComponent } from './components/loading-screen/loading-screen.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoadingScreenComponent, HomeComponent, SearchPageComponent, SingleMovieComponent, WatchListComponent],
+  imports: [RouterOutlet, LoadingScreenComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-movie-app';
+  isLoading = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        if (event.url === '/') {
+          this.isLoading = true;
+        }
+      } else if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          this.isLoading = false;
+        }, 200);
+      }
+    });
+  }
 }
