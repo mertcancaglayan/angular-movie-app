db514775f71049a5f806db01d4ed71ec4789a530
M.can <caglayan.mertcan37@gmail.com>
2024-09-06 13:53:15 +0300

    add local storage set

--- src/app/components/layouts/header/header.component.ts
+++ src/app/components/layouts/header/header.component.ts
@@ -10,4 +10,19 @@ import { CommonModule } from '@angular/common';
 })
 export class HeaderComponent {
   @Input() title: string = '';
+  @Input() movie: number | null = null;
+
+  saveToFav() {
+    if (this.movie) {
+      let favMovies = JSON.parse(localStorage.getItem('fav-movies') || '[]');
+
+      if (!favMovies.includes(this.movie)) {
+        favMovies.push(this.movie);
+        localStorage.setItem('fav-movies', JSON.stringify(favMovies));
+        console.log('Film favorilere eklendi:', this.movie);
+      } else {
+        console.log('Film zaten favorilerde.');
+      }
+    }
+  }
 }
